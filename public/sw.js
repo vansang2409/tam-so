e19cb928b28256b955a524c9e7eae914515898e1
Trần Văn Sang <vansang0105@gmail.com>
2026-06-24 16:16:33 +0700

    Tam So v3.83 - Di Chua: nang cap CSS lu huong + ong xam (radial-gradient gia do cong, dot tre, van go) theo phan hoi xau; 373 test

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tamso-v3.82'
+const CACHE = 'tamso-v3.83'
 self.addEventListener('install', e => {
   e.waitUntil((async () => { try { const c = await caches.open(CACHE); await c.add('./') } catch (_) {} self.skipWaiting() })())
 })
