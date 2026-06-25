c9efdd2e4606820c18d9ed651a40981dab738a8e
Trần Văn Sang <vansang0105@gmail.com>
2026-06-25 10:11:49 +0700

    Tam So v3.91 - Di Chua: an thanh cuon SANG (custom scrollbar toan site theo theme sang) khoi nen TOI rieng cua Di Chua - vet sang doc/ngang chi loi qua DevTools desktop Chrome (dien thoai thuc dung overlay scrollbar); 373 test

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tamso-v3.90'
+const CACHE = 'tamso-v3.91'
 self.addEventListener('install', e => {
   e.waitUntil((async () => { try { const c = await caches.open(CACHE); await c.add('./') } catch (_) {} self.skipWaiting() })())
 })
